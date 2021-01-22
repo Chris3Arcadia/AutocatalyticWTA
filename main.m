7a74ae5fb4f6d3cd1d91050d670cb6e01d9ee023
Chris Arcadia <christopher_arcadia@brown.edu>
2021-01-22 10:54:26 -0500

    minor update to header line in the main script

--- main.m
+++ main.m
@@ -1,6 +1,5 @@
 % Main script for training the network described in "Leveraging autocatalytic reactions for chemical-domain image classification"
-% by Chris Arcadia *
-% * (chris3arcadia, christopher_arcadia@brown.edu, Brown University, updated 2021-01-20)
+% by Chris Arcadia (Chris3Arcadia, christopher_arcadia@brown.edu, Brown University, Updated on 2021-01-20)
 
 configure_options
 set_parameters
